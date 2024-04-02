f8787d8538407be82158eee3a0e8e3a2c583f26d
Sanji <chokpichit@poonpin.de>
2024-04-02 14:17:07 +0200

    Kontrollstrukturen02-Dreieck

---
+++ Kontrollstrukturen02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Kontrollstrukturen02;
+
+class Program
+{
+
+    static void ZeicheDreieck(int zahl, string zeichen)
+    {
+        for(int i = 0; i  < zahl; i++)
+        {
+            for (int j = 0; j <= i && j < zahl; j++)
+            {
+                Console.Write(zeichen);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        ZeicheDreieck(8, "* ");
+        
+    }
+}
+
