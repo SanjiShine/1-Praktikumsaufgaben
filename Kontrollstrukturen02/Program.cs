4284758b824f5b040a2405bdce111af42eeb26a9
Sanji <chokpichit@poonpin.de>
2024-04-02 14:49:59 +0200

    Kontrollstrukturen02-Dreieck

--- Kontrollstrukturen02/Program.cs
+++ Kontrollstrukturen02/Program.cs
@@ -3,22 +3,46 @@
 class Program
 {
 
-    static void ZeicheDreieck(int zahl, string zeichen)
+    static void ZeicheDreieck(int zahl, string zeichen = "*", bool centered = false)
     {
-        for(int i = 0; i  < zahl; i++)
+        if (!centered)
         {
-            for (int j = 0; j <= i && j < zahl; j++)
+            for (int i = 0; i < zahl; i++)
             {
-                Console.Write(zeichen);
-            }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(zeichen);
+                }
 
-            Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            for (int i = 0, leerzeichen = zahl-1;  i < zahl; i++, leerzeichen--)
+            {
+
+                for (int j = 0; j < leerzeichen; j++)
+                    Console.Write(' ');
+
+
+                for (int j = 0; j < 2*i-1; j++)
+                {  
+                    Console.Write(zeichen);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 
+
+
     static void Main(string[] args)
     {
-        ZeicheDreieck(8, "* ");
+        ZeicheDreieck(8);
+        Console.WriteLine();
+        ZeicheDreieck(10, "#", true);
         
     }
 }
