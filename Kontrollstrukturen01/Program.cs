600a6d766a8d2db7a78d424f03082c086cd4e296
Sanji <chokpichit@poonpin.de>
2024-04-02 14:15:18 +0200

    Kontrollstrukturen01-Twist

--- Kontrollstrukturen01/Program.cs
+++ Kontrollstrukturen01/Program.cs
@@ -5,23 +5,22 @@ class Program
 
     static int Twist(int zahl)
     {   
-        int twist = 0;
+        int umgekehrte = 0;
         
             do
             {
                 int ziffer = zahl % 10;
-                twist = twist * 10 + ziffer;
+                umgekehrte = umgekehrte * 10 + ziffer;
                 zahl /= 10;
 
             } while (zahl > 0);
 
-            return twist;
-
+            return umgekehrte;
     }
     static void Main(string[] args)
     { 
-        Console.WriteLine($"Umgekehrte Zahl: {Twist(4567)}"); // 7654
-        Console.WriteLine($"Umgekehrte Zahl: {Twist(1234567890)}"); // 987654321
+        Console.WriteLine($"Umgekehrte Zahl: {Twist(4567)}"); 
+        Console.WriteLine($"Umgekehrte Zahl: {Twist(1234567890)}"); 
 
     }
 }
