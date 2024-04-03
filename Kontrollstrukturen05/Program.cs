086d0b979c82d9c9f4ba67a668135febfc54f21b
Sanji <chokpichit@poonpin.de>
2024-04-03 10:51:49 +0200

    Kontrollstrukturen05-Passwort Validierung

---
+++ Kontrollstrukturen05/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Kontrollstrukturen05;
+
+class Program
+{
+
+    static bool PasswordValidation(string password)
+    {
+        int nZiffer = 0;
+
+        if (password.Length < 8)
+        {
+            return false;
+
+        } else { 
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                char c = password[i];
+
+                if (c < 'A' && c > 'Z' || c > 'a' && c > 'z' || c < '0' && c > '9')
+                {
+                    return false;
+                }
+
+                if (c >= '0' && c <= '9')
+                {
+                    nZiffer++;
+                }
+            }
+
+            if (nZiffer < 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
+      
+    }
+
+    static void Main(string[] args)
+    {
+        string password;
+
+        do
+        {
+            Console.Write("Geben Sie Ihre Passwort bitte ein: ");
+            password = Console.ReadLine();
+
+            if (!PasswordValidation(password))
+            {
+                Console.WriteLine("Ungültiges Passwort!\n");
+            }
+
+        } while (!PasswordValidation(password));
+
+            Console.WriteLine("Gültiges Passwort!");
+
+    }
+}
