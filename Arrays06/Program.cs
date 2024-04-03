62617565c6aa38346ce51718e2b41e420785ab6d
Sanji <chokpichit@poonpin.de>
2024-04-03 17:42:29 +0200

    Arrays Aufgabe06

---
+++ Arrays06/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays06;
+
+class Program
+{
+    public static double SumColumn(double[,] m, int columnIndex)
+    {
+        double sumColumn = 0;
+
+        for(int i = 0; i < m.GetLength(0); i++)
+        {
+              sumColumn += m[i, columnIndex];        
+        }
+        return sumColumn;
+
+    }
+    static void Main(string[] args)
+    {
+        double[,] matrix1 = { { 1, 2, 3 }, { 2, 3, 4 }, { 4, 5, 6 } };
+
+        Console.WriteLine(SumColumn(matrix1, 0)); // Column 1
+        Console.WriteLine(SumColumn(matrix1, 1)); // Column 2
+
+    }
+}
+
