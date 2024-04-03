f5a3ba813319659da951fa9fac021400217437fc
Sanji <chokpichit@poonpin.de>
2024-04-03 17:32:07 +0200

    Arrays Aufgabe11

---
+++ Arrays11/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays11;
+
+class Program
+{
+    public static void RotateValues(int[] values, int rotate)
+    {
+        int[] result = new int[values.Length];
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (rotate+i < values.Length)
+            {
+                result[rotate + i] = values[i];
+
+            }
+
+            Console.Write(result[i] + " ");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array1 = { 1, 2, 3, 4, 5 };
+
+        RotateValues(array1, 2);
+    }
+}
+
