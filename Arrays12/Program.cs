9e56df5c7c8abcfe87b683ad8c65a30cfe399093
Sanji <chokpichit@poonpin.de>
2024-04-03 17:29:29 +0200

    Arrays Aufgabe12

---
+++ Arrays12/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Arrays12;
+
+class Program
+{
+
+    public static int SamePositionSameNumber(int[] a, int[] b)
+    {
+        int samenumber = 0;
+        int checkLength = a.Length;
+        if (b.Length < a.Length)
+        {
+
+            checkLength = b.Length;
+        }
+
+        for (int i = 0; i < checkLength; i++)
+        {
+            if (a[i] == b[i])
+            {
+                samenumber++;
+            }
+        }
+
+        return samenumber;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] a = { 2, 3, 4, 5, 6, 7 };
+        int[] b = { 1, 3, 4, 7, 9 };
+
+        Console.WriteLine(SamePositionSameNumber(a,b));
+
+    }
+}
+
