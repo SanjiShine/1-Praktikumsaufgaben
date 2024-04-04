2941f24373b86bf80802994a21fa0d92a699749e
Sanji <chokpichit@poonpin.de>
2024-04-04 13:02:24 +0200

    Hochschule in einer eigenen Datei

---
+++ Klassen09/Hochschule.cs
@@ -0,0 +1,30 @@
+﻿namespace Klassen09;
+
+class Hochschule
+{
+    static void Main()
+    {
+        
+        Student s1 = new Student("San");
+        Student s2 = new Student("Sanji", 1990);
+        Student s3 = new Student("San Shine", 1999, 10101);
+        Student s4 = new Student("John Doe", "Informatik", 1981, 2534);
+
+        Student[] students = { s1, s2, s3, s4 };
+
+        foreach (Student student in students)
+        {
+            student.Studieren();
+            student.Schlafen();
+            student.Studieren();
+            student.Essen();
+        }
+
+        Console.WriteLine(Student.GetAnzahl());
+
+        Console.WriteLine(s4.IsValid());
+
+        Console.WriteLine(s4);
+    }
+}
+
--- Klassen09/Program.cs
+++ Klassen09/Program.cs
@@ -110,31 +110,3 @@ class Student
 
 
 }
-
-class Hochschule
-{
-    static void Main()
-    {
-        Student s1 = new Student("San");
-        Student s2 = new Student("Sanji", 1990);
-        Student s3 = new Student("San Shine", 1999, 10111);
-        Student s4 = new Student("John Doe", "Informatik", 1981, 2534);
-        
-
-        Student[] students = { s1, s2, s3, s4};
-
-        foreach( Student student in students)
-        {
-            student.Studieren();
-            student.Schlafen();
-            student.Studieren();
-            student.Essen();
-        }
-
-        Console.WriteLine(Student.GetAnzahl());
-
-        Console.WriteLine(s3.IsValid());
-
-        Console.WriteLine(s3);
-    }
-}
