8fdb891ebacf70a9e7a01d902f2ba22abaec81f7
Sanji <chokpichit@poonpin.de>
2024-04-09 11:46:51 +0200

    Klassen02

---
+++ Klassen02/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Klassen02;
+
+class Personalausweis
+{
+    int id;
+    static int idLastest = 999;
+    string name;
+
+
+    public Personalausweis(string name)
+    {
+        this.name = name;
+        this.id = ++idLastest;
+    }
+
+    public int GetId()
+    {
+        return this.id;
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+
+
+    static public int GetLastestId()
+    {
+         return idLastest;
+    }
+
+    public override string ToString()
+    {
+        return $"{id}: {name}";
+    }
+
+
+    static void Main(string[] args)
+    {
+        Personalausweis p1 = new Personalausweis("Cero");
+        Personalausweis p2 = new Personalausweis("Uno");
+        Personalausweis p3 = new Personalausweis("Dos");
+
+        Personalausweis[] personalausweis = { p1, p2, p3 };
+
+        foreach(Personalausweis p in personalausweis)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+
+}
+
